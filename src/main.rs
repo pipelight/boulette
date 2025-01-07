9bfc460e19ee6188e97c3e92ae73c03e063de10d
areskill <areskul@areskul.com>
2025-01-07 08:30:44 +0100

    chore(deps): add minimal deps for fancy prompt

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use gethostname::gethostname;
+use inquire::{
+    validator::{StringValidator, Validation},
+    Text,
+};
+
+// Error handling
+use miette::{IntoDiagnostic, Result};
+use std::error::Error;
+
 fn main() {
-    println!("Hello, world!");
+    display_confirmation_dialog().unwrap();
+}
+
+fn validator(input: &str) -> Result<Validation, Box<dyn Error + Send + Sync>> {
+    if input == gethostname() {
+        Ok(Validation::Valid)
+    } else {
+        Ok(Validation::Invalid(
+            "Input doesn't match with hostname! Aborting!".into(),
+        ))
+    }
+}
+fn display_confirmation_dialog() -> Result<()> {
+    let text = format!(
+        "You are on a remote host {:?} through ssh. Do you realy want to shutdown?",
+        gethostname()
+    );
+    let status = Text::new(&text).with_validator(validator).prompt();
+
+    match status {
+        Ok(status) => println!("Executing command!"),
+        Err(err) => println!("Aborting!"),
+    };
+
+    Ok(())
 }
